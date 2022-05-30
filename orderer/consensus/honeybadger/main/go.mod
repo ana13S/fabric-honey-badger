757ccebf1e94558cf36f09396a02d8aac2767a14
vishalmohanty <vishalmohanty97@gmail.com>
2022-05-30 13:12:50 -0700

    Read/write to files

--- orderer/consensus/honeybadger/main/go.mod
+++ orderer/consensus/honeybadger/main/go.mod
@@ -2,10 +2,6 @@ module core
 
 go 1.17
 
-require github.com/apache/thrift v0.16.0
-
-require github.com/vishalmohanty/go_threshenc v0.0.0
-
 replace github.com/vishalmohanty/go_threshenc v0.0.0 => ../crypto/threshenc/go/go_threshenc
 
 require threshsig v0.0.0
@@ -13,20 +9,20 @@ require threshsig v0.0.0
 replace threshsig v0.0.0 => ../crypto/threshsig
 
 require (
-	github.com/cbergoon/merkletree v0.2.0
-	github.com/deckarep/golang-set v1.8.0
+	github.com/hpcloud/tail v1.0.0
+	github.com/juju/fslock v0.0.0-20160525022230-4d5c94c67b4b
 	github.com/klauspost/reedsolomon v1.9.16
 	github.com/niclabs/tcrsa v0.0.5
 	github.com/pebbe/zmq4 v1.2.8
-	github.com/stretchr/testify v1.7.1
-	github.com/vishalmohanty/encryption v0.0.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.6 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
 
 replace github.com/vishalmohanty/encryption v0.0.0 => ../crypto/threshenc/thrift/gen-go/encryption
--- orderer/consensus/honeybadger/main/honeybadger.go
+++ orderer/consensus/honeybadger/main/honeybadger.go
@@ -3,7 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	// "github.com/juju/fslock"
+	"github.com/hpcloud/tail"
+	"github.com/juju/fslock"
 	tcrsa "github.com/niclabs/tcrsa"
 	zmq "github.com/pebbe/zmq4"
 	"log"
@@ -17,6 +18,11 @@ import (
 )
 
 var all_ports = []string{"5000", "5010", "5020", "5030"}
+var files = []string{"5000.txt", "5010.txt", "5020.txt", "5030.txt"}
+var fileLocks = make([]*fslock.Lock, 4)
+var writeFileHandlers = make([](*os.File), 4)
+var readFileHandler *os.File
+var readScanner *bufio.Scanner
 
 var pid int
 var zctx *zmq.Context
@@ -112,21 +118,93 @@ func handleMessageToSelf(hbm hbMessage) {
 	channel <- hbm.msg
 }
 
-func sendMessages(to int, hbm hbMessage) {
-	//Client port that sends messages
-	// socket.Connect("tcp://localhost:" + port)
-	if pid != to {
-		var finalMessage string = hbm.msgType + "_" + strconv.Itoa(hbm.sender) + "_" + hbm.msg
-		fmt.Println("[sendMessages] Sending message ", finalMessage, " to ", to)
-		clients[to].Send(finalMessage, 0)
+// func sendMessages(to int, hbm hbMessage) {
+// 	//Client port that sends messages
+// 	// socket.Connect("tcp://localhost:" + port)
+// 	if pid != to {
+// 		var finalMessage string = hbm.msgType + "_" + strconv.Itoa(hbm.sender) + "_" + hbm.msg
+// 		fmt.Println("[sendMessages] Sending message ", finalMessage, " to ", to)
+// 		clients[to].Send(finalMessage, 0)
 
-		reply, _ := clients[to].Recv(0)
-		fmt.Println("[sendMessages] Received ", reply)
-	} else {
-		fmt.Println("[sendMessages] Sending message ", hbm.msg, " to self.")
-		handleMessageToSelf(hbm)
-		fmt.Println("[sendMessages] Sent message ", hbm.msg, " to self.")
+// 		reply, _ := clients[to].Recv(0)
+// 		fmt.Println("[sendMessages] Received ", reply)
+// 	} else {
+// 		fmt.Println("[sendMessages] Sending message ", hbm.msg, " to self.")
+// 		handleMessageToSelf(hbm)
+// 		fmt.Println("[sendMessages] Sent message ", hbm.msg, " to self.")
+// 	}
+// }
+
+func broadcast_receiver(pid int) {
+	for {
+		for {
+			lockErr := fileLocks[pid].TryLock()
+			if lockErr != nil {
+				fmt.Println("[broadcast_receiver] Falied to acquire lock > "+lockErr.Error(), " for ", files[pid], " sleeping for a second and retrying")
+			} else {
+				break
+			}
+			time.Sleep(5 * time.Second)
+		}
+
+		fmt.Println("[broadcast_receiver] Opening file ", files[pid], " for reading.")
+
+		// optionally, resize scanner's capacity for lines over 64K, see next example
+		for readScanner.Scan() {
+			message := readScanner.Text()
+
+			fmt.Println("[broadcast_receiver] Message to be parse: ", message)
+
+			// Parse the message
+			splitMsg := strings.Split(message, "_")
+			msgType := splitMsg[0]
+			sender, _ := strconv.Atoi(splitMsg[1])
+			msg := splitMsg[2]
+
+			if msgType != "IGNORE" {
+				channel := getChannelFromMsg(msgType, sender)
+
+				// Put message in apt channel
+				channel <- msg
+			}
+		}
+
+		if err := readScanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("[broadcast_receiver] Messages read from file ", files[pid], ". Deleting file contents and Releasing the lock")
+		// release the lock
+		// if err := os.Truncate(files[pid], 0); err != nil {
+		// 	log.Printf("Failed to truncate: %v", err)
+		// }
+		fileLocks[pid].Unlock()
+		time.Sleep(5 * time.Second)
 	}
+}
+
+func sendMessages(to int, hbm hbMessage) {
+	var finalMessage string = hbm.msgType + "_" + strconv.Itoa(hbm.sender) + "_" + hbm.msg + "\n"
+	fmt.Println("[sendMessages] Sending message ", finalMessage, " to ", to)
+	// for {
+	// 	lockErr := fileLocks[to].TryLock()
+	// 	if lockErr != nil {
+	// 		fmt.Println("[sendMessages] Falied to acquire lock > "+lockErr.Error(), " for ", files[to], " sleeping for a second and retrying")
+	// 	} else {
+	// 		break
+	// 	}
+	// 	time.Sleep(time.Second)
+	// }
+
+	_, err2 := writeFileHandlers[to].WriteString(finalMessage)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println("[sendMessages] Message sent. Releasing the lock")
+	// release the lock
+	// fileLocks[to].Unlock()
 }
 
 func send(to int, msg hbMessage) {
@@ -139,36 +217,36 @@ func broadcast(hbm hbMessage) {
 	}
 }
 
-func broadcast_receiver(s *zmq.Socket) {
-	// s, _ := zctx.NewSocket(zmq.REP)
-	// s.Bind("tcp://*:" + serverPort)
+// func broadcast_receiver(s *zmq.Socket) {
+// 	// s, _ := zctx.NewSocket(zmq.REP)
+// 	// s.Bind("tcp://*:" + serverPort)
 
-	for {
-		// Wait for next request from client
-		message, _ := s.Recv(0)
-		fmt.Println("[broadcast_receiver] Received ", message)
+// 	for {
+// 		// Wait for next request from client
+// 		message, _ := s.Recv(0)
+// 		fmt.Println("[broadcast_receiver] Received ", message)
 
-		// Parse the message
-		splitMsg := strings.Split(message, "_")
-		msgType := splitMsg[0]
-		sender, _ := strconv.Atoi(splitMsg[1])
-		msg := splitMsg[2]
+// 		// Parse the message
+// 		splitMsg := strings.Split(message, "_")
+// 		msgType := splitMsg[0]
+// 		sender, _ := strconv.Atoi(splitMsg[1])
+// 		msg := splitMsg[2]
 
-		if msgType != "IGNORE" {
-			channel := getChannelFromMsg(msgType, sender)
+// 		if msgType != "IGNORE" {
+// 			channel := getChannelFromMsg(msgType, sender)
 
-			// Put message in apt channel
-			channel <- msg
-		}
+// 			// Put message in apt channel
+// 			channel <- msg
+// 		}
 
-		// Do some 'work'
-		time.Sleep(time.Second * 1)
+// 		// Do some 'work'
+// 		time.Sleep(time.Second * 1)
 
-		// Send reply back to client
-		fmt.Println("[broadcast_receiver] Sending Ignore message as reply")
-		s.Send("IGNORE_"+strconv.Itoa(pid), 0)
-	}
-}
+// 		// Send reply back to client
+// 		fmt.Println("[broadcast_receiver] Sending Ignore message as reply")
+// 		s.Send("IGNORE_"+strconv.Itoa(pid)+"_"+strconv.Itoa(sender), 0)
+// 	}
+// }
 
 func (hb *honeybadger) run_round(r int, txn string, hb_block chan []string, receiver *zmq.Socket) {
 	fmt.Println("[run_round] Running round ", r, " proposed txn: ", txn)
@@ -212,18 +290,20 @@ func (hb *honeybadger) run_round(r int, txn string, hb_block chan []string, rece
 		go reliablebroadcast(sid+"RBC"+strconv.Itoa(j), hb.pid, hb.N, hb.f, j, my_rbc_input, rbc_recvs[j], rbc_outputs[j])
 	}
 
-	// for j := 0; j < hb.N; j++ {
-	// 	setup(j)
-	// }
-	setup(0)
+	for j := 0; j < hb.N; j++ {
+		setup(j)
+	}
+	// setup(0)
+	// setup(1)
 
 	rbc_values := make([]chan string, hb.N)
 
 	fmt.Println("[run_round] Spawning common subset")
 	go commonsubset(hb.pid, hb.N, hb.f, rbc_outputs, aba_inputs, aba_outputs, rbc_values)
 
-	fmt.Println("[run_round] Spawning broadcast receiver")
-	go broadcast_receiver(receiver)
+	// fmt.Println("[run_round] Spawning broadcast receiver")
+	// go broadcast_receiver(receiver)
+	// go broadcast_receiver(hb.pid)
 
 	fmt.Println("[run_round] Adding txn ", txn, " to input channel.")
 	input := make(chan string, 1)
@@ -317,11 +397,11 @@ func main() {
 	receiver, _ := zctx.NewSocket(zmq.REP)
 	defer receiver.Close()
 
-	receiver.Bind("tcp://*:" + all_ports[pid])
+	// receiver.Bind("tcp://*:" + all_ports[pid])
 
-	go sync_nodes(receiver, pid)
+	// go sync_nodes(receiver, pid)
 
-	time.Sleep(5 * time.Second)
+	// time.Sleep(5 * time.Second)
 
 	fmt.Println("[main] buffer: ", transaction_buffer, " N: ", N, " f: ", f, " B: ", B)
 
@@ -338,26 +418,67 @@ func main() {
 		transaction_buffer: transaction_buffer,
 	}
 
+	// for i := 0; i < N; i++ {
+	// 	if i != pid {
+	// 		clients[i], _ = zctx.NewSocket(zmq.REQ)
+	// 		defer clients[i].Close()
+	// 		clients[i].Connect("tcp://localhost:" + all_ports[i])
+
+	// 		fmt.Println("[main] Sending hello to ", i)
+	// 		clients[i].Send("Hello from "+strconv.Itoa(pid), 0)
+
+	// 		msg, _ := clients[i].Recv(0)
+	// 		fmt.Printf("[main] Received reply from %d: %s\n", i, msg)
+	// 	}
+	// }
+
+	// time.Sleep(5 * time.Second)
+
+	// fmt.Println("[main] Waiting for sync go routines to complete")
+	// wg.Wait()
+
+	// time.Sleep(10 * time.Second)
+
 	for i := 0; i < N; i++ {
-		if i != pid {
-			clients[i], _ = zctx.NewSocket(zmq.REQ)
-			defer clients[i].Close()
-			clients[i].Connect("tcp://localhost:" + all_ports[i])
+		fileLocks[i] = fslock.New(files[i])
+		f, err := os.OpenFile(files[i], os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 
-			fmt.Println("[main] Sending hello to ", i)
-			clients[i].Send("Hello from "+strconv.Itoa(pid), 0)
-
-			msg, _ := clients[i].Recv(0)
-			fmt.Printf("[main] Received reply from %d: %s\n", i, msg)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		writeFileHandlers[i] = f
+
+		defer writeFileHandlers[i].Close()
 	}
 
-	time.Sleep(5 * time.Second)
+	readFileHandler, err = os.Open(files[pid])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer readFileHandler.Close()
 
-	fmt.Println("[main] Waiting for sync go routines to complete")
-	wg.Wait()
+	readScanner = bufio.NewScanner(readFileHandler)
 
-	time.Sleep(10 * time.Second)
+	go func() {
+		t, _ := tail.TailFile(files[pid], tail.Config{Follow: true})
+		for line := range t.Lines {
+			message := line.Text
+			fmt.Println("[main] ", message)
+			// Parse the message
+			splitMsg := strings.Split(message, "_")
+			msgType := splitMsg[0]
+			sender, _ := strconv.Atoi(splitMsg[1])
+			msg := splitMsg[2]
+
+			if msgType != "IGNORE" {
+				channel := getChannelFromMsg(msgType, sender)
+
+				// Put message in apt channel
+				channel <- msg
+			}
+		}
+	}()
 
 	fmt.Println("[main] Ready to run honeybadger ", hb.N)
 	hb.run(receiver)
