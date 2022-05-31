b9605b8556aa35f40c2906fd63974a33599d2920
Ana Selvaraj <ana@selvaraj.org>
2022-05-30 19:19:50 -0700

    Add test to generate new keys

---
+++ orderer/consensus/honeybadger/main/generate_keys_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"testing"
+	"threshsig"
+)
+
+func Test_generateKeys(t *testing.T) {
+	shares, meta := threshsig.Dealer(4, 3, 2048)
+	f, err := os.Create("keys.txt")
+
+	if err != nil {
+		log.Fatal("Could not create keys.txt")
+	}
+
+	sharesJson, _ := json.Marshal(shares)
+	metaJson, _ := json.Marshal(meta)
+
+	f.Write(metaJson)
+	f.WriteString(string('\n'))
+	f.Write(sharesJson)
+	f.WriteString(string('\n'))
+
+	defer f.Close()
+}
